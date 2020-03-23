5c773dacf1285bae1e9acef10399bbcefb8455e1
aurelbnn <aurel.bonneau1507@gmail.com>
2020-03-23 13:12:19 +0100

    :sparkles: Definition of WebGL Zone

--- src/index.js
+++ src/index.js
@@ -1,1 +1,115 @@
 import './style/main.styl'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+/**
+ * Sizes
+ */
+const sizes = {}
+sizes.width = window.innerWidth
+sizes.height = window.innerHeight
+
+/**
+ * Cursor
+ */
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
+
+window.addEventListener('mousemove', (_event) =>
+{
+    cursor.x = _event.clientX / sizes.width - 0.5
+    cursor.y = _event.clientY / sizes.height - 0.5
+})
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene()
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 20)
+camera.position.z = 8
+scene.add(camera)
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3)
+directionalLight.position.x = 7
+scene.add(directionalLight)
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
+scene.add(hemisphereLight)
+
+/**
+ * Objects
+ */
+const objectsGroup = new THREE.Group()
+scene.add(objectsGroup)
+
+const material = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+})
+
+// Sphere
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(1, 16, 16), material)
+sphere.position.x = 0
+objectsGroup.add(sphere)
+
+
+// Floor
+const floor = new THREE.Mesh(new THREE.PlaneBufferGeometry(20, 20, 1, 1), material)
+floor.position.y = - 3
+floor.rotation.x -= Math.PI * 0.5
+objectsGroup.add(floor)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(window.devicePixelRatio)
+document.body.appendChild(renderer.domElement)
+
+/**
+ * Camera Controls
+ */
+const cameraControls = new OrbitControls(camera, renderer.domElement)
+cameraControls.zoomSpeed = 0.3
+cameraControls.enableDamping = true
+
+/**
+ * Resize
+ */
+window.addEventListener('resize', () =>
+{
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+})
+
+/**
+ * Loop
+ */
+const loop = () =>
+{
+    window.requestAnimationFrame(loop)
+
+    // Camera controls
+    cameraControls.update()
+
+    // Render
+    renderer.render(scene, camera)
+}
+
+loop()
