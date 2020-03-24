576d32c9b5979dcd97ca7359a548d217856927e1
PESauvage <sauvage.paulemile@gmail.com>
2020-03-24 12:41:48 +0100

    :construction: Imported floor texture

--- src/javascript/Room.js
+++ src/javascript/Room.js
@@ -1,4 +1,14 @@
 import * as THREE from 'three'
+import woodFloorColorSource from '../images/WoodFloor/WoodfloorColor.jpg'
+import woodFloorNormalSource from '../images/WoodFloor/WoodfloorNormal.jpg'
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+
+const woodfloorColorTexture = textureLoader.load(woodFloorColorSource)
+const woodfloorNormalTexture = textureLoader.load(woodFloorNormalSource)
 
 export default class Room
 {
@@ -82,6 +92,7 @@ export default class Room
         const floor = new THREE.Mesh(new THREE.PlaneBufferGeometry(30, 30, 1, 1), material)
         floor.position.y = 0
         floor.rotation.x -= Math.PI * 0.5
+        
 
         _scene.add(wallFirstMesh, wallSecondMesh, wallThirdMesh, wallFourthMesh, wallFifthMesh, wallSixthMesh, wallSeventhMesh, wallEighthMesh, wallNinthMesh, wallTenthMesh, wallEleventhMesh, wallTwelfthMesh, floor)
     }
