6cd8e5cc8cd7916d81dfbd402d2adcaab3eb7b83
aurelbnn <aurel.bonneau1507@gmail.com>
2020-03-24 15:16:55 +0100

    :art: Changing of class name

--- src/javascript/Joconde.js
+++ src/javascript/Joconde.js
@@ -21,36 +21,36 @@ export default class Room
             color: 0x04072C,
         })
 
-        const jocondeWallPartOne = new THREE.Mesh(new THREE.BoxGeometry( 1.25, 4, 0.1 ), wallsMaterial)
-        jocondeWallPartOne.position.x = 4.375
-        jocondeWallPartOne.position.y = 2
-        jocondeWallPartOne.position.z = 8
+        const jocondeWallPartOneMesh = new THREE.Mesh(new THREE.BoxGeometry( 1.25, 4, 0.1 ), wallsMaterial)
+        jocondeWallPartOneMesh.position.x = 4.375
+        jocondeWallPartOneMesh.position.y = 2
+        jocondeWallPartOneMesh.position.z = 8
 
-        const jocondeWallPartTwo = new THREE.Mesh(new THREE.BoxGeometry( 1.25, 4, 0.1 ), wallsMaterial)
-        jocondeWallPartTwo.position.x = 0.625
-        jocondeWallPartTwo.position.y = 2
-        jocondeWallPartTwo.position.z = 8
+        const jocondeWallPartTwoMesh = new THREE.Mesh(new THREE.BoxGeometry( 1.25, 4, 0.1 ), wallsMaterial)
+        jocondeWallPartTwoMesh.position.x = 0.625
+        jocondeWallPartTwoMesh.position.y = 2
+        jocondeWallPartTwoMesh.position.z = 8
 
-        const jocondeWallPartThree = new THREE.Mesh(new THREE.BoxGeometry( 2.5, 1, 0.1 ), wallsMaterial)
-        jocondeWallPartThree.position.x = 2.5
-        jocondeWallPartThree.position.y = 3.5
-        jocondeWallPartThree.position.z = 8
+        const jocondeWallPartThreeMesh = new THREE.Mesh(new THREE.BoxGeometry( 2.5, 1, 0.1 ), wallsMaterial)
+        jocondeWallPartThreeMesh.position.x = 2.5
+        jocondeWallPartThreeMesh.position.y = 3.5
+        jocondeWallPartThreeMesh.position.z = 8
 
-        const jocondeWallPartFour = new THREE.Mesh(new THREE.BoxGeometry( 2.5, 1, 0.1 ), wallsMaterial)
-        jocondeWallPartFour.position.x = 2.5
-        jocondeWallPartFour.position.y = 0.5
-        jocondeWallPartFour.position.z = 8
+        const jocondeWallPartFourMesh = new THREE.Mesh(new THREE.BoxGeometry( 2.5, 1, 0.1 ), wallsMaterial)
+        jocondeWallPartFourMesh.position.x = 2.5
+        jocondeWallPartFourMesh.position.y = 0.5
+        jocondeWallPartFourMesh.position.z = 8
 
-        const jocondeFloor = new THREE.Mesh(new THREE.BoxGeometry( 5, 0.1, 2 ), wallsMaterial)
-        jocondeFloor.position.x = 2.5
-        jocondeFloor.position.y = 0.1
-        jocondeFloor.position.z = 9
+        const jocondeFloorMesh = new THREE.Mesh(new THREE.BoxGeometry( 5, 0.1, 2 ), wallsMaterial)
+        jocondeFloorMesh.position.x = 2.5
+        jocondeFloorMesh.position.y = 0.1
+        jocondeFloorMesh.position.z = 9
 
-        const jocondeCeiling = new THREE.Mesh(new THREE.BoxGeometry( 5.1, 0.1, 2.1 ), wallsMaterial)
-        jocondeCeiling.position.x = 2.5
-        jocondeCeiling.position.y = 4
-        jocondeCeiling.position.z = 9
+        const jocondeCeilingMesh = new THREE.Mesh(new THREE.BoxGeometry( 5.1, 0.1, 2.1 ), wallsMaterial)
+        jocondeCeilingMesh.position.x = 2.5
+        jocondeCeilingMesh.position.y = 4
+        jocondeCeilingMesh.position.z = 9
         
-        _scene.add(jocondeWallPartOne, jocondeWallPartTwo, jocondeWallPartThree, jocondeWallPartFour, jocondeFloor, jocondeCeiling)
+        _scene.add(jocondeWallPartOneMesh, jocondeWallPartTwoMesh, jocondeWallPartThreeMesh, jocondeWallPartFourMesh, jocondeFloorMesh, jocondeCeilingMesh)
     }
 }
